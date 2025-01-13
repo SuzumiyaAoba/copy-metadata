1e717db0b75e2bbb19cfbfd4fca34fa4c6f498b3
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2025-01-13 17:02:13 +0900

    init commit

--- background/messages/openOptions.ts
+++
@@ -1,9 +0,0 @@
-import type { PlasmoMessaging } from "@plasmohq/messaging";
-
-const handler: PlasmoMessaging.MessageHandler = async (req, _res) => {
-  if (req.name === "openOptions") {
-    chrome.runtime.openOptionsPage();
-  }
-};
-
-export default handler;
