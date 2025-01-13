e0fc1b89ed188126aea119adf1f3b07869b626e0
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2025-01-13 18:10:39 +0900

    chore: move files

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import "./popup.css";
+import Popup from "./Popup.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Popup />
+  </StrictMode>
 );
